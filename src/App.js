51810cf731f0a428ef9d4b44d791b0837b2e9db0
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-02-07 21:56:51 -0500

    added navigation to hats page using useNavigate

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div>
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route path='/hats' element={<HatsPage />} />
+        <Route path='/shop/hats' element={<HatsPage />} />
       </Routes>
       
     </div>
