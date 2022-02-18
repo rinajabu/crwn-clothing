40b20bc0d1c7361419cd300b9e74e080f0dd7fa1
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-02-17 21:25:04 -0500

    header component created with links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/Header.component';
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
