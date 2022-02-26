14015ca901167b818e8ee357b2453eaf98da158c
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-02-26 00:01:45 -0500

    added redirect on user sign in, added types file to user redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import Header from './components/header/Header.component';
@@ -42,15 +42,26 @@ class App extends React.Component {
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
-          <Route path='/signin' element={<SignInAndSignUp />} />
+          <Route 
+            exact 
+            path='/signin' 
+            element={ this.props.currentUser 
+              ? <Navigate to='/' push />
+              : <SignInAndSignUp />
+              } 
+          />
         </Routes>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
