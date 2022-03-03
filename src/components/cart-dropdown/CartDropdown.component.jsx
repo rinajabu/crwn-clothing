ca2116c3c84c824e5f0c059e781c12879a90904b
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-03-03 09:03:35 -0500

    added reselect library to application, updated cart with memoized selectors

--- src/components/cart-dropdown/CartDropdown.component.jsx
+++ src/components/cart-dropdown/CartDropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { selectCartItems } from '../../redux/cart/cart-selectors'
 import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/CustomButton.component'
 import CartItem from '../cart-item/CartItem.component'
@@ -17,8 +18,8 @@ const CartDropdown = ({ cartItems }) => {
     )
 }
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
